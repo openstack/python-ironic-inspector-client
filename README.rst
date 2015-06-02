cd38f11acbaa505bf064a818221fb988bc10def7
Dmitry Tantsur <dtantsur@redhat.com>
2015-06-02 17:09:03 +0200

    Initial import

---
+++ README.rst
@@ -0,0 +1,19 @@
+Ironic-Inspector Client
+=======================
+
+This is a client library and tool for ironic-inspector_.
+
+Install from PyPI::
+
+    pip install python-ironic-inspector-client
+
+Please refer to the `Inspector README`_ for details on usage.
+
+Please follow usual OpenStack `Gerrit Workflow`_ to submit a patch, see
+`Inspector contributing guide`_ for more detail.
+
+
+.. _Gerrit Workflow: http://docs.openstack.org/infra/manual/developers.html#development-workflow
+.. _ironic-inspector: https://pypi.python.org/pypi/ironic-discoverd
+.. _Inspector README: https://github.com/stackforge/ironic-discoverd/blob/master/README.rst
+.. _Inspector contributing guide: https://github.com/stackforge/ironic-discoverd/blob/master/CONTRIBUTING.rst
---
+++ setup.py
@@ -0,0 +1,46 @@
+import re
+
+from setuptools import setup
+
+
+try:
+    # Distributions have to delete *requirements.txt
+    with open('requirements.txt', 'r') as fp:
+        install_requires = [re.split(r'[<>=~]', line)[0]
+                            for line in fp if line.strip()]
+except EnvironmentError:
+    print("No requirements.txt, not handling dependencies")
+    install_requires = []
+
+
+with open('ironic_inspector_client/__init__.py', 'rb') as fp:
+    exec(fp.read())
+
+
+setup(
+    name = "python-ironic-inspector-client",
+    version = __version__,
+    description = open('README.rst', 'r').readline().strip(),
+    author = "Dmitry Tantsur",
+    author_email = "dtantsur@redhat.com",
+    url = "https://pypi.python.org/pypi/python-ironic-inspector-client",
+    packages = ['ironic_inspector_client', 'ironic_inspector_client.test'],
+    install_requires = install_requires,
+    entry_points = {
+        'openstack.cli.extension': [
+            'baremetal-introspection = ironic_inspector_client.shell',
+        ],
+        'openstack.baremetal_introspection.v1': [
+            "baremetal_introspection_start = ironic_inspector_client.shell:StartCommand",
+            "baremetal_introspection_status = ironic_inspector_client.shell:StatusCommand",
+        ],
+    },
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: OpenStack',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+    ],
+    license = 'APL 2.0',
+)
